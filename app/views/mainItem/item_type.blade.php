77bbad7bf9da950a419257fc9c1d60fa38a14918
JaimeDia <44525513+JaimeDia@users.noreply.github.com>
2019-02-10 19:44:24 +0800

    Changed "Item Name" to "Item Details"

--- app/views/mainItem/item_type.blade.php
+++ app/views/mainItem/item_type.blade.php
@@ -4,13 +4,13 @@
       <!-- Content Header (Page header) -->
           <section class="content-header">
             <h1>
-              Maintenance <small>Item Name</small>
+              Maintenance <small>Item Details</small>
               
             </h1>
             <ol class="breadcrumb">
               <li><a href="<?php echo 'maintenanceMenu' ?>"><i class="fa fa-dashboard"></i> Maintenance</a></li>
               <li><a href="#">Item</a></li>
-              <li class="active">Item Name</li>
+              <li class="active">Item Details</li>
             </ol>
           </section>
 
@@ -21,7 +21,7 @@
                 <!-- general form elements -->
                   <div class="box box-primary">
                     <div class="box-header with-border">
-                      <h3 class="box-title">Item Name</h3>
+                      <h3 class="box-title">Item Details</h3>
                     </div><!-- /.box-header -->
                     <!-- form start -->
                     <form role="form" onsubmit="return false" action = "{{URL::to('addItemType')}}" enctype="multipart/form-data" id = "addForm">
